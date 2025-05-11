24467f4eb5b1ec7e9857cef4a940784b139c3414
lauracolladoq <laucolqui@gmail.com>
2025-05-11 21:44:47 +0200

    feat: configure Supabase client with environment variables

---
+++ .env.d.ts
@@ -0,0 +1,4 @@
+declare module "@env" {
+  export const EXPO_SUPABASE_URL: string;
+  export const EXPO_SUPABASE_ANNON_KEY: string;
+}
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,12 @@
+import { AppState } from "react-native";
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+// @ts-ignore
+import { EXPO_SUPABASE_URL, EXPO_SUPABASE_ANNON_KEY } from "@env";
 
-const supabaseUrl = "https://nxbqerkivppqchbyhmge.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54YnFlcmtpdnBwcWNoYnlobWdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2OTI3MzgsImV4cCI6MjA1OTI2ODczOH0.MiVOt0qIJk5I4qwkYjj5y7TJT_8y_wQ4mxbYN-4VSXI";
+const supabaseUrl = EXPO_SUPABASE_URL;
+const supabaseAnonKey = EXPO_SUPABASE_ANNON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
@@ -12,4 +15,17 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     detectSessionInUrl: false,
   },
+});
+
+// Tells Supabase Auth to continuously refresh the session automatically
+// if the app is in the foreground. When this is added, you will continue
+// to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
+// `SIGNED_OUT` event if the user's session is terminated. This should
+// only be registered once.
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
 });

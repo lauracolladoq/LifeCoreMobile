4c147a1ffcf067f0717c8f01625c5e1a2f890c8f
lauracolladoq <laucolqui@gmail.com>
2025-05-30 08:50:33 +0200

    feat: update font loading to use individual Nunito font files

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,28 +1,34 @@
 import { Stack } from "expo-router";
 import "../global.css";
-import { useFonts } from 'expo-font';
-import { useEffect } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 const RootLayout = () => {
-  const [loaded, error] = useFonts({
-    'Nunito': require('../assets/fonts/Nunito-VariableFont_wght.ttf'),
+  const [fontsLoaded] = useFonts({
+    "Nunito-Light": require("../assets/fonts/Nunito-Light.ttf"),
+    "Nunito-SemiBold": require("../assets/fonts/Nunito-Semi-Bold.ttf"),
+    "Nunito-Bold": require("../assets/fonts/Nunito-Bold.ttf"),
   });
+
   useEffect(() => {
-    if (loaded || error) {
+    if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
-  }, [loaded, error]);
+  }, [fontsLoaded]);
 
-  if (!loaded && !error) {
+  if (!fontsLoaded) {
     return null;
   }
 
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{
-        headerShown: false,
-      }}/>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 };
